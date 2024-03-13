565369caa538b3728598357e7cb04d7581efce0e
Domin C <dominikcedro00@gmail.com>
2024-03-13 11:18:46 +0100

    ADD methods for total income, expense and expense/income by category in Analyze class

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 class Expense:
     """
-    This class represents an expense. It has a name, date, category, and value.
+    This class represents a single expense. It has a name, date, category, and value.
 
     ATTRIBUTES:
     name: str
@@ -41,6 +41,32 @@ class Analyze:
         self.expenses = expenses # should be a list of expenses (from a single reciept)
         self.incomes = incomes # should be a list of incomes (paychecks, other incomes)
 
-        def total_expenses(self):
-            return sum(expense.value for expense in self.expenses)
+    def total_expenses(self):
+        return sum(expense.value for expense in self.expenses)
 
+    def total_income(self):
+        return sum(income.value for income in self.incomes)
+
+    def total_expense_category(self, category):
+        sum = 0
+        for expense in self.expenses:
+            if expense.category == category:
+                sum += expense.value
+        return sum
+
+    def total_income_category(self, category):
+        sum = 0
+        for income in self.incomes:
+            if income.category == category:
+                sum += income.value
+        return sum
+
+    #TODO: add a method to calculate total expenses by category
+
+    #TODO: add a method to calculate total expenses by date
+
+    #TODO: add a method to calculate total income by date
+
+    #TODO: add a method to calculate total income by category
+
+    #TODO: write unit tests for each method
---
+++ main_test.py
@@ -0,0 +1,22 @@
+import unittest
+from main import Expense, Income, Analyze
+
+class TestAnalyze(unittest.TestCase):
+    def setUp(self):
+        self.exp1 = Expense('Rent', '2022-01-01', 'Housing', 1000.0)
+        self.exp2 = Expense('Groceries', '2022-01-02', 'Food', 200.0)
+        self.inc1 = Income('Salary', '2022-01-01', 'Job', 3000.0)
+        self.analyze = Analyze([self.exp1, self.exp2], [self.inc1])
+
+    def test_total_expenses(self):
+        self.assertEqual(self.analyze.total_expenses(), 1200.0)
+
+    def test_total_income(self):
+        self.assertEqual(self.analyze.total_income(), 3000.0)
+
+    def test_total_expense_category(self):
+        self.assertEqual(self.analyze.total_expense_category('Housing'), 1000.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
